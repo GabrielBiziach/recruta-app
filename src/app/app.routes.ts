3e4460fcdc181ffa1fff2912ed935c6fee83e0a8
gabrielbiziach <gabrielbiziach@gmail.com>
2024-05-30 00:03:18 -0300

    feat: criação inicial da tela de login, admin e user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent }, // Criaremos o AdminComponent em seguida
+  { path: 'user', component: UserComponent },   // Criaremos o UserComponent em seguida
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
